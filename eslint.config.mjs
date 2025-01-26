c489f340d9b6ced7702260a65589c2eaf92a4d4d
Gregori <greg.gego@gmail.com>
2025-01-26 14:57:30 +0100

    eslint configuration set

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,76 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next",
+    "next/core-web-vitals",
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:prettier/recommended"
+  ),
+  {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+  },
+  {
+    plugins: ["react", "prettier"],
+  },
+  {
+    rules: {
+      "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "never",
+          jsx: "never",
+          ts: "never",
+          tsx: "never",
+        },
+      ],
+      "react/function-component-definition": [
+        2,
+        {
+          namedComponents: "arrow-function",
+          unnamedComponents: "arrow-function",
+        },
+      ],
+      "react/require-default-props": "off",
+      "react/prop-types": "off",
+      "react/jsx-props-no-spreading": "off",
+      "no-unused-vars": "off",
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    overrides: [
+      {
+        files: ["*.tsx"],
+        rules: {
+          "react/require-default-props": "off",
+        },
+      },
+    ],
+  },
 ];
 
 export default eslintConfig;
