3ce9878aad81f08a90e49ce20ea40852a6d74d08
Gregori <greg.gego@gmail.com>
2025-01-26 15:32:04 +0100

    added about, contact & projects pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
-    </div>
-  );
-}
+const Home = () => {
+  return <div className={styles.wrapper}>Home</div>;
+};
+
+export default Home;
