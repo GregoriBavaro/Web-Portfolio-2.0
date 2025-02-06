76b74fd50cd24a0c2614d65a253bae5276f68cac
Gregori <greg.gego@gmail.com>
2025-02-06 21:57:35 +0100

    changed home page a bit to make it more fit with the rest of the pages

--- src/components/layout/hero/Hero.tsx
+++ src/components/layout/hero/Hero.tsx
@@ -7,25 +7,28 @@ import { INFORMATION } from "../../../db/hero";
 const Hero = () => {
   return (
     <section className={styles.hero}>
+      <div className={styles.hero__line}></div>
       <div className={styles.hero__intro}>
-        <span>Hello, I am</span>
-        <h1>Gregori Bavaro</h1>
-        <h4>
-          <IoIosArrowForward /> Web Developer
-        </h4>
-      </div>
-      <div className={styles.hero__info}>
-        <ul>
-          {INFORMATION.map(({ id, title, text, info }) => (
-            <li key={id}>
-              <h5>{title}</h5>
-              <p>
-                <span>const</span>
-                <span>{text}</span>=<span>{info}</span>
-              </p>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <span>Hello, I am</span>
+          <h1>Gregori Bavaro</h1>
+          <h4>
+            <IoIosArrowForward /> Web Developer
+          </h4>
+        </div>
+        <div className={styles["hero__intro-info"]}>
+          <ul>
+            {INFORMATION.map(({ id, title, text, info }) => (
+              <li key={id}>
+                <h5>{title}</h5>
+                <p>
+                  <span>const</span>
+                  <span>{text}</span>=<span>{info}</span>
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
